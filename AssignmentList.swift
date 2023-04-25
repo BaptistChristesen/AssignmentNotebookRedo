8a38224f2b8c946f1f994bb97bce4711a5b0aa4c
sol gudmin <115568958+schnappiiiiiiii@users.noreply.github.com>
2023-04-25 13:17:44 -0500

    finished

--- AssignmentList.swift
+++ AssignmentList.swift
@@ -7,7 +7,22 @@
 
 import Foundation
  class AssignmentList: ObservableObject {
-     @Published var items = [AssignmentItem(course: "Math", description: "HWK", dueDate: Date()),
-                             AssignmentItem(course: "English", description: "Sollilloquy analysis", dueDate: Date()),
-                             AssignmentItem(course: "Data Sturctures", description: "Catching Plagiraists", dueDate: Date())]
+     @Published var items : [AssignmentItem] {
+              didSet {
+                  let encoder = JSONEncoder()
+                  if let encoded = try? encoder.encode(items) {
+                      UserDefaults.standard.set(encoded, forKey: "Data")
+                  }
+              }
+          }
+          init() {
+              if let items = UserDefaults.standard.data(forKey: "Data") {
+                  let decoder = JSONDecoder()
+                  if let decoded = try? decoder.decode([AssignmentItem].self, from: items) {
+                      self.items = decoded
+                      return
+                  }
+              }
+              items = []
+          }
  }
