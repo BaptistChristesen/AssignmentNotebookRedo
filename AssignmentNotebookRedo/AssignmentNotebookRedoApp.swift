e33eebfe407b19dc0ba3669e2f146b47e9dd98ee
sol gudmin <115568958+schnappiiiiiiii@users.noreply.github.com>
2023-04-25 11:21:25 -0500

    Initial Commit

---
+++ AssignmentNotebookRedo/AssignmentNotebookRedoApp.swift
@@ -0,0 +1,17 @@
+//
+//  AssignmentNotebookRedoApp.swift
+//  AssignmentNotebookRedo
+//
+//  Created by caden christesen on 4/25/23.
+//
+
+import SwiftUI
+
+@main
+struct AssignmentNotebookRedoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
